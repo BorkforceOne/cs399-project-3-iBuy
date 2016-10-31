75edd9438bb7e4355d424210080236c1fefe88e9
Harrison Lambeth <harrison@nau.edu>
2016-10-30 23:57:57 -0700

    Added drawer

--- js/Scenes/MainScene.js
+++ js/Scenes/MainScene.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import { View, TouchableHighlight, StyleSheet } from 'react-native';
+import { View, TouchableHighlight, StyleSheet, Text as ReactText } from 'react-native';
 import { Container, Button, List, ListItem, Header, Title, Icon, Footer, FooterTab, Content, Badge, Text } from 'native-base';
+import Drawer from 'react-native-drawer';
+import MainDrawer from '../Drawers/MainDrawer';
 import '../Utils/NumberHelpers';
 
 export default class MainScene extends Component {
@@ -22,8 +24,14 @@ export default class MainScene extends Component {
                     purchaser: null,
                     category: 'Household - Maintenance'
                 }
-            ]
+            ],
+            showDrawer: false
         }
+    }
+    onShowDrawer() {
+        this.setState({
+            showDrawer: true
+        })
     }
     render() {
 
@@ -39,35 +47,65 @@ export default class MainScene extends Component {
         });
 
         return (
-            <Container>
-                <Header>
-                    <Button transparent>
-                        <Icon name="md-menu" />
-                    </Button>
-                    <Title>Item View</Title>
-                </Header>
-                <Content>
-                    <List>
-                        {items}
-                    </List>
-                </Content>
-                <Footer>
-                    <FooterTab>
-                        <Button>
-                            Items
-                            <Icon name="md-cart" />
+            <Drawer
+                open={this.state.showDrawer}
+                type="overlay"
+                openDrawerOffset={0.2}
+                panCloseMask={0.2}
+                closedDrawerOffset={-3}
+                styles={drawerStyles}
+                content={<MainDrawer {...this.props}/>}
+                elevation={10}
+                tweenHandler={(ratio) => ({
+                    main: { opacity: (2-ratio/2) },
+                    mainOverlay: { opacity:ratio/2 }
+                })}
+                >
+                <Container>
+                    <Header>
+                        <Button transparent onPress={this.onShowDrawer.bind(this)}>
+                            <Icon name="md-menu" />
                         </Button>
-                        <Button>
-                            Group Members
-                            <Icon name="md-person" />
+                        <Title>Item View</Title>
+                    </Header>
+                    <Content>
+                        <List>
+                            {items}
+                        </List>
+                    </Content>
+                    <View style={styles.addButton}>
+                        <Button rounded primary textStyle={{lineHeight: 20}}>
+                            <Icon name="md-person-add" />
                         </Button>
-                    </FooterTab>
-                </Footer>
-            </Container>
+                    </View>
+                    <Footer>
+                        <FooterTab>
+                            <Button>
+                                Items
+                                <Icon name="md-cart"/>
+                            </Button>
+                            <Button>
+                                Groups
+                                <Icon name="md-people"/>
+                            </Button>
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            </Drawer>
         )
     }
 }
 
-const styles = StyleSheet.create({
+const drawerStyles = {
+    drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3 },
+    mainOverlay: { backgroundColor: 'black' },
+    main: { paddingLeft: 3 }
+};
 
+const styles = StyleSheet.create({
+    addButton: {
+        position: "absolute",
+        right: 25,
+        bottom: 80
+    }
 });
