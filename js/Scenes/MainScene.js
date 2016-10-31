aa9c24d10bcab0f1522c8e4728f5868e99bfa596
Brandon Garling <bjg96@nau.edu>
2016-10-31 00:14:52 -0700

    Add menu dropdown

--- js/Scenes/MainScene.js
+++ js/Scenes/MainScene.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { View, TouchableHighlight, StyleSheet } from 'react-native';
 import { Container, Button, List, ListItem, Header, Title, Icon, Footer, FooterTab, Content, Badge, Text } from 'native-base';
+import Menu, { MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
+
 import '../Utils/NumberHelpers';
 
 export default class MainScene extends Component {
@@ -25,6 +27,23 @@ export default class MainScene extends Component {
             ]
         }
     }
+    openMenu() {
+        this.refs.MenuContext.openMenu("menu");
+    }
+
+    gotoScene(id) {
+        if (id == "sign-in") {
+            this.props.navigator.resetTo({
+                id: id
+            });
+        }
+        else {
+            this.props.navigator.push({
+                id: id
+            });
+        }
+    }
+
     render() {
 
         let items = this.state.items.map((item, i) => {
@@ -39,35 +58,57 @@ export default class MainScene extends Component {
         });
 
         return (
-            <Container>
-                <Header>
-                    <Button transparent>
-                        <Icon name="md-menu" />
-                    </Button>
-                    <Title>Item View</Title>
-                </Header>
-                <Content>
-                    <List>
-                        {items}
-                    </List>
-                </Content>
-                <Footer>
-                    <FooterTab>
-                        <Button>
-                            Items
-                            <Icon name="md-cart" />
+            <MenuContext style={{ flex: 1 }} ref="MenuContext">
+                <Menu name="menu" style={styles.moreMenu} onSelect={this.gotoScene.bind(this)}>
+                    <MenuTrigger>
+                    </MenuTrigger>
+                    <MenuOptions>
+                        <MenuOption value="about">
+                            <Text>About</Text>
+                        </MenuOption>
+                        <MenuOption value="sign-in">
+                            <Text>Log Out</Text>
+                        </MenuOption>
+                    </MenuOptions>
+                </Menu>
+                <Container>
+                    <Header>
+                        <Button transparent>
+                            <Icon name="md-menu" />
                         </Button>
-                        <Button>
-                            Group Members
-                            <Icon name="md-person" />
+                        <Title>Item View</Title>
+                        <Button transparent onPress={this.openMenu.bind(this)}>
+                            <Icon name="md-more" />
                         </Button>
-                    </FooterTab>
-                </Footer>
-            </Container>
+                    </Header>
+                    <Content>
+
+                        <List>
+                            {items}
+                        </List>
+                    </Content>
+                    <Footer>
+                        <FooterTab>
+                            <Button>
+                                Items
+                                <Icon name="md-cart" />
+                            </Button>
+                            <Button>
+                                Group Members
+                                <Icon name="md-person" />
+                            </Button>
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            </MenuContext>
         )
     }
 }
 
 const styles = StyleSheet.create({
-
+    moreMenu: {
+        position: 'absolute',
+        top: 5,
+        right: 5
+    }
 });
