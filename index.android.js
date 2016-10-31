81a2535aa160c5144c4b54d77eb21fd6208fb9a9
Brandon Garling <bjg96@nau.edu>
2016-10-30 17:07:45 -0700

    Add app navigation

--- index.android.js
+++ index.android.js
@@ -5,49 +5,47 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
+import SignInScene from './js/Scenes/SignInScene.js';
 
 export default class iBuy extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    /**
+     * Render the app
+     */
+    render() {
+        // Simply divert the rendering to renderScene()
+        return (
+            <Navigator
+                initialRoute={{id: 'sign-in'}}
+                renderScene={this.renderScene.bind(this)}
+            />
+        );
+    }
+
+    /**
+     * Decide which root level component to render depending on which scene is currently being accessed
+     * @param route - The route to render
+     * @param navigator - The navigator
+     */
+    renderScene(route, navigator) {
+
+        let params = {
+            route: route,
+            navigator: navigator
+        };
+
+        switch (route.id) {
+            // Render the calculator
+            case "sign-in":
+                return (
+                    <SignInScene {...params}/>
+                );
+        }
+    }
+}
 
 AppRegistry.registerComponent('iBuy', () => iBuy);
