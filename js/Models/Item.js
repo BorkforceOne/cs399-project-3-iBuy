82440b827ee7470487af189b3262b694563236dc
Brandon Garling <bjg96@nau.edu>
2016-11-12 20:25:05 -0700

    Update default Item creation properties

--- js/Models/Item.js
+++ js/Models/Item.js
@@ -1,14 +1,24 @@
 /**
  * Created by Brandon Garling on 11/9/2016.
  */
+import Moment from 'moment';
+
+let nextId = -1;
 export default class Item {
     Id;
-    PurchaserId;
-    GroupId;
-    Name;
-    Quantity;
-    Cost;
-    Category;
-    Due;
-    Color;
+    PurchaserId = null;
+    GroupId = null;
+    Name = "";
+    Quantity = 1;
+    Cost = 1;
+    Category = "";
+    Due = Moment();
+    Color = "#00f";
+
+    constructor(id) {
+        if (id === undefined)
+            this.Id = nextId--;
+        else
+            this.Id = id;
+    }
 }
